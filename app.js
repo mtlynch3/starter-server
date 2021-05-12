e35adc752912dc500c67d44406e5742f44274d43
Justin Kuang <justin.kuang63@myhunter.cuny.edu>
2021-05-12 11:05:14 -0400

    Add env file, change port back to default 500

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 //import database setup utils
 const createDB = require('./database/utils/createDB');
 const seedDB = require('./database/utils/seedDB');
+// const PORT = process.env.PORT;
 
 // Instantiate express application
-const express = require("express");
+const express = require('express');
 const app = express();
 
 //our database instance
@@ -12,18 +13,17 @@ const db = require('./database');
 //express router
 const apiRouter = require('./routes/index');
 
-
 const syncDatabase = async () => {
   //sync and seed
   try {
-    await db.sync({force: true});
-    console.log('------Synced to db--------')
+    await db.sync({ force: true });
+    console.log('------Synced to db--------');
     await seedDB();
     console.log('--------Successfully seeded db--------');
   } catch (err) {
     console.error('syncDB error:', err);
-  }  
-}
+  }
+};
 
 const configureApp = async () => {
   // handle request data
@@ -31,26 +31,25 @@ const configureApp = async () => {
   app.use(express.urlencoded({ extended: false }));
 
   // Mount apiRouter
-  app.use("/api", apiRouter);
+  app.use('/api', apiRouter);
 
   // Handle page not found:
   // gets triggered when a request is made to
-  // an undefined route 
+  // an undefined route
   app.use((req, res, next) => {
-    const error = new Error("Not Found, Please Check URL!");
+    const error = new Error('Not Found, Please Check URL!');
     error.status = 404;
     next(error);
   });
 
-  // Error-handling middleware: 
+  // Error-handling middleware:
   // all express errors get passed to this
   // when next(error) is called
   app.use((err, req, res, next) => {
     console.error(err);
     console.log(req.originalUrl);
-    res.status(err.status || 500).send(err.message || "Internal server error.");
+    res.status(err.status || 500).send(err.message || 'Internal server error.');
   });
-
 };
 
 const bootApp = async () => {
@@ -61,6 +60,11 @@ const bootApp = async () => {
 
 bootApp();
 
-
 const PORT = 5000;
 app.listen(PORT, console.log(`Server started on ${PORT}`));
+
+// if (PORT) {
+//   app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+// } else {
+//   console.log('===== ERROR ====\nCREATE A .env FILE!\n===== /ERROR ====');
+// }
