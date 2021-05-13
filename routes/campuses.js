2165cd684893f4da70ddfa3f04e05140ac090e26
Justin Kuang <justin.kuang63@myhunter.cuny.edu>
2021-05-13 10:28:06 -0400

    Add routes to add, edit and delete a Campus

--- routes/campuses.js
+++ routes/campuses.js
@@ -3,19 +3,46 @@ const router = express.Router();
 const { Student, Campus } = require('../database/models');
 
 const ash = require('express-async-handler');
+const { noExtendLeft } = require('sequelize/types/lib/operators');
 
 /** GET ALL CAMPUSES */
-
-router.get('/', ash(async(req, res) => {
-  let campuses = await Campus.findAll({include: [Student]});
-  res.status(200).json(campuses);
-}));
+router.get(
+  '/',
+  ash(async (req, res) => {
+    let campuses = await Campus.findAll({ include: [Student] });
+    res.status(200).json(campuses);
+  })
+);
 
 /** GET CAMPUS BY ID*/
-router.get('/:id', ash(async(req, res) => {
-  let campus = await Campus.findByPk(req.params.id, {include: [Student]});
-  res.status(200).json(campus);
-}));
+router.get(
+  '/:id',
+  ash(async (req, res) => {
+    let campus = await Campus.findByPk(req.params.id, { include: [Student] });
+    res.status(200).json(campus);
+  })
+);
+
+/** ADD NEW CAMPUS **/
+router.post('/', function (req, res, next) {
+  Campus.create(req.body)
+    .then((createdCampus) => res.status(200).json(createdCampus))
+    .catch((err) => next(err));
+});
+
+/** EDIT A CAMPUS  **/
+router.put('/:id', async (req, res) => {
+  await Campus.update(req.body, { where: { id: req.params.id } });
+  let campus = await Campus.findByPk(req.params.id);
+  res.status(201).json(campus);
+});
+
+/** DELETE A CAMPUS **/
+router.delete('/:id', function (req, res, next) {
+  Campus.destroy({ where: { id: req.params.id } })
+    .then(() => res.status(200).json('Deleted a campus!'))
+    .catch((err) => next(err));
+});
 
 // Export our router, so that it can be imported to construct our apiRouter;
 module.exports = router;
--- routes/students.js
+++ routes/students.js
@@ -25,44 +25,51 @@ const ash = require('express-async-handler');
 /** GET ALL STUDENTS: express-async-handler (ash) */
 // automatically catches any error and sends to middleware
 // same as using try/catch and calling next(error)
-router.get('/', ash(async(req, res) => {
-  let students = await Student.findAll({include: [Campus]});
-  res.status(200).json(students);
-}));
+router.get(
+  '/',
+  ash(async (req, res) => {
+    let students = await Student.findAll({ include: [Campus] });
+    res.status(200).json(students);
+  })
+);
 
 /** GET STUDENT BY ID */
-router.get('/:id', ash(async(req, res) => {
-  let student = await Student.findByPk(req.params.id, {include: [Campus]});
-  res.status(200).json(student);
-}));
+router.get(
+  '/:id',
+  ash(async (req, res) => {
+    let student = await Student.findByPk(req.params.id, { include: [Campus] });
+    res.status(200).json(student);
+  })
+);
 
 /** ADD NEW STUDENT */
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
   Student.create(req.body)
-    .then(createdStudent => res.status(200).json(createdStudent))
-    .catch(err => next(err));
+    .then((createdStudent) => res.status(200).json(createdStudent))
+    .catch((err) => next(err));
 });
 
 /** DELETE STUDENT */
-router.delete('/:id', function(req, res, next) {
+router.delete('/:id', function (req, res, next) {
   Student.destroy({
     where: {
-      id: req.params.id
-    }
+      id: req.params.id,
+    },
   })
-    .then(() => res.status(200).json("Deleted a student!"))
-    .catch(err => next(err));
+    .then(() => res.status(200).json('Deleted a student!'))
+    .catch((err) => next(err));
 });
 
 /******************* EDIT *********************/
 
-router.put('/:id', ash(async(req, res) => {
-  await Student.update(req.body,
-        { where: {id: req.params.id} }
-  );
-  let student = await Student.findByPk(req.params.id);
-  res.status(201).json(student);
-}));
+router.put(
+  '/:id',
+  ash(async (req, res) => {
+    await Student.update(req.body, { where: { id: req.params.id } });
+    let student = await Student.findByPk(req.params.id);
+    res.status(201).json(student);
+  })
+);
 
 // Export our router, so that it can be imported to construct our apiRouter;
 module.exports = router;
