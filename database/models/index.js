f0d14cc7fd84fad5207858e33860f58fe7f9329c
Justin Kuang <justin.kuang63@myhunter.cuny.edu>
2021-05-20 11:40:18 -0400

    changed index.js

--- database/models/index.js
+++ database/models/index.js
@@ -1,7 +1,7 @@
 // register models, set up associations between tables, and generate barrel file for the models;
 
-const Student = require('./Student.js');
-const Campus = require('./Campus.js');
+const Student = require('./Student');
+const Campus = require('./Campus');
 
 Student.belongsTo(Campus);
 Campus.hasMany(Student);
