2f8bb321500855555b72c6fc6b7927d229ea34c3
winson65 <winson.yu22@gmail.com>
2021-05-21 23:55:22 -0400

    added addition fields for student and campus, fixed seedDB to include them

--- database/utils/seedDB.js
+++ database/utils/seedDB.js
@@ -3,17 +3,25 @@ const { Campus, Student } = require('../models');
 const seedDB = async () => {
 	const dummy_campus = await Campus.create({
 		name: "Hunter College",
-		description: "This is a school in NYC."
+		description: "This is a school in NYC.",
+    address : "68th Street Lexington Avenue",
+    imageUrl : "https://image.shutterstock.com/image-vector/male-avatar-profile-picture-use-600w-193292036.jpg"
 	});
+  
 	const dummy_campus2 = await Campus.create({
 		name: "Harvard",
-		description: "This is a school in MA."
+    address : "123 East 5th Street",
+		description: "This is a school in MA.",
+    imageUrl : "https://image.shutterstock.com/image-vector/male-avatar-profile-picture-use-600w-193292036.jpg"
 	});
 
 	const dummy_student = await Student.create({
-			firstname: "Joe",
+      firstname: "Joe",
       lastname: "Shmo",
-		});
+      imageUrl : "https://image.shutterstock.com/image-vector/male-avatar-profile-picture-use-600w-193292036.jpg",
+      email : "testemail@gmail.com",
+      gpa : 4.0
+  });
 
 	await dummy_student.setCampus(dummy_campus);
 	
