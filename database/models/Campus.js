46175bdb5623d6bc1a580ef20c574dd69e09b55d
Shafali Gupta <shafaligupta31@gmail.com>
2021-12-10 14:55:39 -0500

    added campus

--- database/models/Campus.js
+++ database/models/Campus.js
@@ -6,22 +6,21 @@ const Campus = db.define("campus", {
   name: {
     type: Sequelize.STRING,
     allowNull: false
+  },
+
+  imageUrl:{
+    type: Sequelize.STRING,
+    defaultValue:"https://i.picsum.photos/id/875/200/300.jpg?hmac=9NSoqXHP89pGlq4Sz3OgGxjx5c91YHJkcIOBFgNJ8xA"
+  },
+
+  address: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+
+  description:{
+    type:Sequelize.TEXT('long')
   }
-  // ,
-
-  // imageUrl:{
-  //   type: URL,
-  //   defaultValue:" "
-  // },
-
-  // address: {
-  //   type: Sequelize.STRING,
-  //   allowNull: false
-  // },
-
-  // description:{
-  //   type:Sequelize.TEXT('long')
-  // }
 });
 
 module.exports = Campus;
