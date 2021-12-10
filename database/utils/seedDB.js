05763ac19e2f5b5e2c2334d5dd01fbb47e5cbecd
Shafali Gupta <shafaligupta31@gmail.com>
2021-12-10 14:32:09 -0500

    fixed URL error

--- database/utils/seedDB.js
+++ database/utils/seedDB.js
@@ -3,16 +3,25 @@ const { Campus, Student } = require('../models');
 const seedDB = async () => {
 	const dummy_campus = await Campus.create({
 		name: "Hunter College",
+		imageUrl: "https://i.picsum.photos/id/875/200/300.jpg?hmac=9NSoqXHP89pGlq4Sz3OgGxjx5c91YHJkcIOBFgNJ8xA",
+		address: "141 E 25th street", 
 		description: "This is a school in NYC."
+		
+
 	});
 	const dummy_campus2 = await Campus.create({
 		name: "Harvard",
+		imageUrl: "https://i.picsum.photos/id/875/200/300.jpg?hmac=9NSoqXHP89pGlq4Sz3OgGxjx5c91YHJkcIOBFgNJ8xA",
+		address: "280 E 30th street", 
 		description: "This is a school in MA."
 	});
 
 	const dummy_student = await Student.create({
 			firstname: "Joe",
       lastname: "Shmo",
+	  email: "hello@gmail.com",
+	  imageUrl: "https://i.picsum.photos/id/690/200/200.jpg?hmac=DN6slU20ktSeMSXbM6U8BG_YHhebxEl3S70qNurkzk8",  
+	  gpa: 3.3
 		});
 
 	await dummy_student.setCampus(dummy_campus);
