b3d8e7386c39a1fb9edc53c747cc936d467c88e3
JayJay458 <89559217+JayJay458@users.noreply.github.com>
2021-12-14 23:13:39 -0500

    Campus adding new campus

--- database/models/Campus.js
+++ database/models/Campus.js
@@ -8,8 +8,13 @@ const Campus = db.define("campus", {
     allowNull: false
   },
 
+  address:{
+    type:Sequelize.STRING,
+    allowNull:false
+  },
+
   description: {
-    type: Sequelize.STRING,
+    type: Sequelize.STRING(8000),
   },
 
   imageUrl:{
@@ -17,9 +22,6 @@ const Campus = db.define("campus", {
     defaultValue:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.kindpng.com%2Fimgv%2FobRJJT_user-profile-avatar-login-account-male-user-icon%2F&psig=AOvVaw1m2ZA3Xu_-rIkE5MIKapHO&ust=1639355448848000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOib5IuB3fQCFQAAAAAdAAAAABAD "
   },
 
-  description:{
-    type:Sequelize.STRING(5000)
-  }
 });
 
 module.exports = Campus;
